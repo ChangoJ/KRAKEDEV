71bc7b2ff27b6db3d1a66b300432772688ad7441
JordanChango <jordan-chango@hotmail.com>
2024-01-27 07:55:44 -0500

    funcion recuperarTexto ok

--- ejerciciogit/ejercicio.js
+++ ejerciciogit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar = function(){
+    let cmpImagenSaludo
+    cmpImagenSaludo = document.getElementById("imgSaludo")
+    cmpImagenSaludo.src = "imagenes/jaguar.jpg"
     console.log("ingresa a saludar")
 }
