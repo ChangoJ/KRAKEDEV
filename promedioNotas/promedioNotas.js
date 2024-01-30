e22893bbabfc3a0893cfb45207481d48de2b1925
MR.CHANGO <jordanchango25000@gmail.com>
2024-01-29 21:32:01 -0500

    Funciones condiciones notas ok

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -1,4 +1,4 @@
-calcularPromedioNotas = function(){
+calcularPromedioNotas = function () {
     let nota1
     let nota2
     let nota3
@@ -7,13 +7,24 @@ calcularPromedioNotas = function(){
     nota2 = recuperarFloat("txtNota2")
     nota3 = recuperarFloat("txtNota3")
 
-    promedio = calcularPromedio(nota1,nota2,nota3)
+    promedio = calcularPromedio(nota1, nota2, nota3)
 
-    mostrarTexto("lblPromedio",promedio.toFixed(2))
+    mostrarTexto("lblPromedio", promedio.toFixed(2))
 
-    if(promedio > 7){
+    if (promedio < 5 && promedio > 0) {
+        mostrarTexto("lblMensaje", "REPROBADO")
+        mostrarImagen("imgSituacion", "imagenes/fracaso.gif")
+
+    } else if (promedio >= 5 && promedio <= 8) {
+        mostrarTexto("lblMensaje", "BUEN TRABAJO")
         mostrarImagen("imgSituacion", "imagenes/exito.gif")
+
+    } else if (promedio > 8 && promedio <= 10) {
+        mostrarTexto("lblMensaje", "EXCELENTE")
+        mostrarImagen("imgSituacion", "imagenes/excelente.gif")
     }else{
+        
+        mostrarTexto("lblMensaje", "DATOS INCORRECTOS")
         mostrarImagen("imgSituacion", "imagenes/fracaso.gif")
     }
 }
