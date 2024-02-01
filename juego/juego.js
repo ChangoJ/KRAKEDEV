1de5c9dcf7330800d0c23b8bc2abcb6fce7a5a57
MR.CHANGO <jordanchango25000@gmail.com>
2024-01-31 21:56:23 -0500

    Funciones papel, piedra o tijera funcionando

--- juego/juego.js
+++ juego/juego.js
@@ -8,7 +8,41 @@ jugar = function(eleccionJugador){
     let ganadorDeterminado
     elementoGenerado = generarElemento()
     ruta = generarRuta(elementoGenerado)
-    console.log(elementoGenerado)
     ganadorDeterminado = determinarGanador(eleccionJugador,elementoGenerado)
-    console.log(ganadorDeterminado)
+   
+    mostrarImagen("imgChooseComputer",ruta )
+
+    mostrarImagen("imgChooseUser","imagenes/"+eleccionJugador+".png" )
+
+    if(ganadorDeterminado == 0){
+        mostrarTexto("lblMessage", "EMPATE")
+    }else if(ganadorDeterminado == 1){
+        mostrarTexto("lblMessage", "GANASTE LA PARTIDA")
+        puntosUsuario++
+        mostrarTexto("lblpuntosUsuario", puntosUsuario)
+    }else if(ganadorDeterminado == 2){
+        mostrarTexto("lblMessage", "PERDISTE LA PARTIDA")
+        puntosComputador++
+        mostrarTexto("lblpuntosComputador",puntosComputador)
+    }
+
+    if(puntosUsuario == 5){
+        alert("HAS GANADO EL JUEGO")
+    }else if(puntosComputador == 5){
+        alert("EL COMPUTADOR A GANADO")
+    }
+
+
+}
+
+limpiar = function(){
+    mostrarTexto("lblMessage", "")
+    mostrarTexto("lblpuntosUsuario", 0)
+    mostrarTexto("lblpuntosComputador", 0)
+
+    mostrarImagen("imgChooseComputer","" )
+    mostrarImagen("imgChooseUser","" )
+
+    puntosUsuario = 0
+    puntosComputador = 0
 }
