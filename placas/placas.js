f3dedd64b15c7687b7cf940f79d61442dfd0e6db
MR.CHANGO <jordanchango25000@gmail.com>
2024-01-31 20:02:25 -0500

    AÑADIDO BOTON LIMPIAR

--- placas/placas.js
+++ placas/placas.js
@@ -1,4 +1,4 @@
-validarPlaca = function(){
+validarPlaca = function () {
     let erroresEstructura
     let placa
     let provincia
@@ -7,29 +7,29 @@ validarPlaca = function(){
     placa = recuperarTexto("txtPlaca")
 
     erroresEstructura = validarEstructura(placa)
-    if(erroresEstructura == null){
+    if (erroresEstructura == null) {
         erroresEstructura = "\nESTRUCTURA VALIDA\n"
         provincia = obtenerProvincia(placa)
         tipoVehiculo = obtenerTipoVehiculo(placa)
         picoYPlaca = obtenerDiaPicoYPlaca(placa)
 
-        if(provincia == null){
+        if (provincia == null) {
             mostrarTexto("lblValidation-provincia", "Provincia Incorrecta")
-        }else {
-            mostrarTexto("lblValidation-provincia", provincia+"\n")
+        } else {
+            mostrarTexto("lblValidation-provincia", provincia + "\n")
         }
 
-        if(tipoVehiculo == null){
+        if (tipoVehiculo == null) {
             mostrarTexto("lblValidation-vehiculo", "Vehiculo privado\n")
-        }else {
-            mostrarTexto("lblValidation-vehiculo", tipoVehiculo+"\n")
+        } else {
+            mostrarTexto("lblValidation-vehiculo", tipoVehiculo + "\n")
         }
 
-        if(picoYPlaca){
+        if (picoYPlaca) {
             mostrarTexto("lblValidation-picoYPlaca", picoYPlaca)
         }
-    }else{
-        erroresEstructura = "\nESTRUCTURA INCORRECTA: "+erroresEstructura+"\n"
+    } else {
+        erroresEstructura = "\nESTRUCTURA INCORRECTA: " + erroresEstructura + "\n"
         mostrarTexto("lblValidation-provincia", "")
         mostrarTexto("lblValidation-vehiculo", "")
         mostrarTexto("lblValidation-picoYPlaca", "")
@@ -38,4 +38,13 @@ validarPlaca = function(){
 
 
     mostrarTexto("lblValidation-placa", erroresEstructura)
+}
+
+limpiar = function () {
+    mostrarTextoEnCaja("txtPlaca", "")
+    mostrarTexto("lblValidation-placa", "")
+    mostrarTexto("lblValidation-provincia", "")
+    mostrarTexto("lblValidation-vehiculo", "")
+    mostrarTexto("lblValidation-picoYPlaca", "")
+
 }
