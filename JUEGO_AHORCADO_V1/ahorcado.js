e4fbef0de583006f29f33554014dd6b01e836395
MR.CHANGO <jordanchango25000@gmail.com>
2024-02-01 20:40:55 -0500

    PASO 7

--- JUEGO_AHORCADO_V1/ahorcado.js
+++ JUEGO_AHORCADO_V1/ahorcado.js
@@ -67,8 +67,11 @@ ingresarLetra = function () {
         validar(letra)
         if (coincidencias == 5) {
             alert("HA GANADO")
+            mostrarImagen("ahorcadoImagen", "ganador.gif")
+       
         } else if (intentos == 10) {
             alert("HA PERDIDO")
+            mostrarImagen("ahorcadoImagen", "gameOver.gif")
         }
     } else {
         alert("SOLO SE ACEPTAN MAYUSCULAS")
